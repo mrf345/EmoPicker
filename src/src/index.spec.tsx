6b6c7997105eeed40d8fa2c7b7ae5de73e232bd3
Mohamed Feddad <mrf345@gmail.com>
2020-03-09 16:54:13 +0300

    Remove set styles. Add settings snapshot test.

--- src/src/index.spec.tsx
+++ src/src/index.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import Main from './index'
+import Settings from './components/Settings'
 
 describe('Main component sanity checks 🧑‍🔧', () => {
     const self = {component: render(<Main />)}
@@ -10,5 +11,19 @@ describe('Main component sanity checks 🧑‍🔧', () => {
 
     test('Check main component snapshot', () => {
         expect(self.component).toMatchSnapshot()
+    })
+
+    test('Check settings component snapshot', () => {
+        const settings = {shortcut: 'testing',
+                          backgroundColor: 'testing',
+                          fontColor: 'testing',
+                          afterSelection: 'nothing'}
+        const afterSelection = {'nothing': {action: () => {},
+                                            description: 'Do nothing'}}
+
+        expect(render(<Settings settings={settings}
+                                applySettings={() => {}}
+                                handleStateChange={() => {}}
+                                afterSelection={afterSelection}/>)).toMatchSnapshot()
     })
 })
